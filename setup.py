08f000c8f3cb938ddfcd35dbded20cf6f3a4d916
Robert Klep <robert@klep.name>
2011-07-26 18:10:25 +0200

    Added 2.2.4 to appcast

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
             ],
             # settings for Sparkle
             SUFeedURL = 'http://quotefix.quotefixformac.googlecode.com/hg/updates/appcast.xml',
+            #SUFeedURL = 'http://localhost:8000/appcast.xml',
             SUEnableAutomaticChecks = True,
             SUShowReleaseNotes = True,
             SUPublicDSAKeyFile = 'quotefix.sparkle.pub.pem',
