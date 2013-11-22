68da44468511d955545d9db3cd4c87f0fff8927c
Robert Klep <robert@klep.name>
2013-11-22 08:19:11 +0100

    Added UUID for 10.9.1 build 13B35

--- setup.py
+++ setup.py
@@ -83,8 +83,10 @@ setup(
                 '72F55B63-35E4-4323-8CA7-104774A0DE95',
                 # Mavericks Mail update
                 'FBE5B158-5602-4A6D-9CC5-8461B9B7054E',
-                # 10.9.1 build 13b27
+                # 10.9.1 build 13B27
                 'E5EEAF89-45D0-4AB2-94C1-F1771B141573',
+                # 10.9.1 build 13B35
+                '1CD40D64-945D-4D50-B12D-9CD865533506',
             ],
             # settings for Sparkle
             SUFeedURL = 'https://raw.github.com/robertklep/quotefixformac/master/updates/appcast.xml',
